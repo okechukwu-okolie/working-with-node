a7a4b83b1d36093a79a3fea7bb1bf9097f79869d
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-11-19 18:14:30 -0800

    working with nodejs

---
+++ test.js
@@ -0,0 +1,23 @@
+// const name = 'mario'
+// console.log(name);
+
+// const greet = (name)=>{
+//     console.log(`hello, ${name}`);
+    
+// }
+
+// greet('samuel')
+// greet('elizabeth')
+
+//global object
+// console.log(global);
+
+global.setTimeout(() => {
+    console.log('hello world');
+    clearInterval(int)
+}, 3000);
+
+const int = setInterval(() => {
+    console.log('hello world now');
+    
+}, 1000);
